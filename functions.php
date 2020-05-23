7095eaf1c461ac98eefa3085d7c0e6167cee8118
Eugene Kim <eugene@MacBook-Pro.local>
2020-05-23 14:49:59 -0700

    First commit

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function followeugene_theme_support() {
+    // Adds dynamic title tag support
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'followeugene_theme_support');
+
+function followeugene_register_styles() {
+
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('followeugene-style', get_template_directory_uri() . "/style.css", array('followeugene-boostrap'), $version, 'all');
+    wp_enqueue_style('followeugene-boostrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('followeugene-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'followeugene_register_styles');
+
+function followeugene_register_scripts() {
+
+    wp_enqueue_script('followeugene-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('followeugene-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('followeugene-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+    wp_enqueue_script('followeugene-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'followeugene_register_scripts');
+
+?>
