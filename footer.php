7f2c01ebdb4c09b15d499fdfdae2c1ed186c5626
Eugene Kim <eugene@MacBook-Pro.local>
2020-05-24 23:47:17 -0700

    updating changes

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+           <p class="copyright"><a href="https://eugenetedkim.com">FollowEugene</a></p>
+          
+       </footer>
+
 <?php
     wp_footer();
 ?>
--- header.php
+++ header.php
@@ -58,4 +58,8 @@
 
 			</div>
 		</nav>
-    </header>
+	</header>
+	<div class="main-wrapper">
+	    <header class="page-title theme-bg-light text-center gradient py-5">
+			<h1 class="heading">Eugene's Blog Home Page Heading</h1>
+		</header>
