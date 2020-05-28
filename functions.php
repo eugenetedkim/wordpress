e6861ba283855aae605b3b32828e1e67864767b7
Eugene Kim <eugene@MacBook-Pro.local>
2020-05-27 23:27:53 -0700

    saving files

--- functions.php
+++ functions.php
@@ -3,9 +3,22 @@
 function followeugene_theme_support() {
     // Adds dynamic title tag support
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'followeugene_theme_support');
+
+function followeugene_menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'followeugene_menus');
 
 function followeugene_register_styles() {
 
